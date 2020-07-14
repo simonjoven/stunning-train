b24cc3b540a7a1b67a495f8abf4f6c30dad13846
simonjoven <68258686+simonjoven@users.noreply.github.com>
2020-07-14 15:22:18 +0800

    Add the main file to the repo.

---
+++ SetupWizard.cpp
@@ -0,0 +1,25 @@
+#include<iostream>
+#include<string>
+#include<Windows.h>
+using namespace std;
+
+int main() {
+	string welcomeWizard;
+
+	cout << "This is the installer wizard for Aqua Cloud Computing Service.\n";
+	cout << "Do you want to continue?";
+
+	cin >> welcomeWizard;
+
+	if (welcomeWizard == "User input") {
+		// Code when user inputs something, insert code here
+	}
+	else if (welcomeWizard == "User input") {
+		// Code when user inputs something, insert code here
+	}
+	else {
+		// Code when nothing matches, insert code here
+	}
+
+	// End of code, so far
+}
